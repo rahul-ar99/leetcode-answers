78186d73f9cb91c21946ef81c3ccb1670d9e0aa6
rahul-ar99 <rahulararukattu@gmail.com>
2024-01-25 09:14:51 +0530

    new question

---
+++ Qno:11.js
@@ -0,0 +1,23 @@
+// contain most water
+
+
+
+function waterContainer(inp){
+
+    maximum_water = 0
+
+    for(let i in inp){
+        
+        for(let j=i;(j<inp.length);j++){
+            if(inp[i]<inp[j]){
+                inp[i]
+            }
+        }x
+
+    }
+}
+
+
+const a= [1,8,6,2,5,4,8,3,7]
+waterContainer(a)
+
